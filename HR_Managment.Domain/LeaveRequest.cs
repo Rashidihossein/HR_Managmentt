072946dc2a2e623517fab99be33c1f05442ac909
hossein <hossein@DESKTOP-H4PJ0T3>
2023-04-29 21:27:56 +0430

    Add project files.

---
+++ HR_Managment.Domain/LeaveRequest.cs
@@ -0,0 +1,22 @@
+﻿using HR_Managment.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Managment.Domain
+{
+    public class LeaveRequest:BaseEntity
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public DateTime DateRequested { get; set; }
+        public string RequestComments { get; set; }
+        public DateTime DateActioned { get; set; }
+        public bool? Aproved { get; set; }
+        public bool Cancelled { get; set; }
+    }
+}
---
+++ HR_Managment.Domain/LeaveType.cs
@@ -0,0 +1,19 @@
+﻿using HR_Managment.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Managment.Domain
+{
+    public class LeaveType : BaseEntity
+    {
+       
+        public string Name { get; set; }
+        public int DefaultDay { get; set; }
+        
+
+
+    }
+}
