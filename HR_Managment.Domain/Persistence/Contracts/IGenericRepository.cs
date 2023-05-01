4809e72f87ed3de3fe9f95b2d496ccb33fc1aef9
hossein rsh <hossein.rsh6013@gmail.com>
2023-05-01 21:22:01 +0430

    ساخت Repository برای entity ها

--- HR_Managment.Domain/Persistence/Contracts/IGenericRepository.cs
+++ HR_Managment.Domain/Persistence/Contracts/IGenericRepository.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 
 namespace HR_Managment.Domain.Persistence.Contracts
 {
-    public interface IGenericRepository<T> where T : class 
+    public interface IGenericRepository<T> where T : class
     {
+        Task<T> Get(int id);
+        Task<IReadOnlyList<T>> GetAll();
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
+
 
     }
-   
+
 }
---
+++ HR_Managment.Domain/Persistence/Contracts/ILeaveAllocationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Managment.Domain.Persistence.Contracts
+{
+    public interface ILeaveAllocationRepository:IGenericRepository<LeaveAllocation>
+    {
+
+
+    }
+}
