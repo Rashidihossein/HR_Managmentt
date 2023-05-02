fdc7083e2a212cb352694f4b02c0f094536e9303
hossein rsh <hossein.rsh6013@gmail.com>
2023-05-02 21:44:54 +0430

    اضافه کردن IGenericRepository

--- HR_Managment.Domain/Persistence/Contracts/IGenericRepository.cs
+++ HR_Managment.Domain/Persistence/Contracts/IGenericRepository.cs
@@ -8,7 +8,11 @@ namespace HR_Managment.Domain.Persistence.Contracts
 {
     public interface IGenericRepository<T> where T : class 
     {
-
+        Task<T> Get(int id);
+        Task<IReadOnlyList<T>> GetAll();
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
     }
    
 }
