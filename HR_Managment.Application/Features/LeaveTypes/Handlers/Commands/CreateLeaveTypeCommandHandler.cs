78ea14fc65b943b709f467496d0bd92dceb53b7b
hossein rsh <hossein.rsh6013@gmail.com>
2023-05-12 22:47:29 +0430

    ساخت Command از CQRS

---
+++ HR_Managment.Application/Features/LeaveTypes/Handlers/Commands/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HR_Managment.Application.Features.LeaveTypes.Requests.Commands;
+using HR_Managment.Application.Persistence.Contracts;
+using HR_Managment.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Managment.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public CreateLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveType = _mapper.Map<LeaveType>(request.LeaveTypeDTO);
+            leaveType = await _leaveTypeRepository.Add(leaveType);
+            return leaveType.Id;
+        }
+    }
+}
---
+++ HR_Managment.Application/Features/LeaveTypes/Requests/Commands/CreateLeaveTypeCommand.cs
@@ -0,0 +1,15 @@
+﻿using HR_Managment.Application.DTOs.LeaveType;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Managment.Application.Features.LeaveTypes.Requests.Commands
+{
+    public class CreateLeaveTypeCommand:IRequest<int>
+    {
+        public LeaveTypeDTO LeaveTypeDTO { get; set; }
+    }
+}
