b007c58525d97d4704f2f682697d98f769c32eb9
hossein rsh <hossein.rsh6013@gmail.com>
2023-05-16 17:00:38 +0430

    Update DTOs

---
+++ HR_Managment.Application/DTOs/LeaveRequest/ChangeLeaveRequestApprovalDTO.cs
@@ -0,0 +1,15 @@
+﻿using HR_Managment.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Managment.Application.DTOs.LeaveRequest
+{
+    public class ChangeLeaveRequestApprovalDTO : BaseDTO
+    {
+        public bool? Aproved { get; set; }
+
+    }
+}
---
+++ HR_Managment.Application/DTOs/LeaveRequest/UpdateLeaveRequestDTO.cs
@@ -0,0 +1,23 @@
+﻿using HR_Managment.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Managment.Application.DTOs.LeaveRequest
+{
+    public class UpdateLeaveRequestDTO : BaseDTO
+    {
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public bool Cancelled { get; set; }
+
+        public string RequestComments { get; set; }
+
+    }
+}
